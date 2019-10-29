251a5848111bce4039e04153bab9121eb39513e7
David Fuentes <D.gabefuentes@gmail.com>
2019-10-29 11:19:48 -0700

    Refactors proxy // moves other services out of proxy repo

--- app-photo-carousel/client/src/main.jsx
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/extensions */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PhotoCarousel from './app.jsx';
-
-ReactDOM.render(<PhotoCarousel />, document.getElementById('PhotoCarousel'));
