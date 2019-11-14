ed8cf71c1f03fb1b099cd664d750549250b2c0a9
Ubuntu <ubuntu@ip-172-31-16-42.us-east-2.compute.internal>
2019-11-14 19:43:36 +0000

    Configurations to proxy made on ec2 instance

--- proxy/public/server.js
+++ proxy/public/server.js
@@ -2,7 +2,7 @@ require('newrelic');
 const express = require("express");
 const morgan = require("morgan");
 const cors = require('cors');
-const proxy = require('express-http-proxy');
+const proxy = require('http-proxy-middleware');
 const app = express();
 const port = 3000;
 
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(morgan("dev"));
 
 app.use(express.static('public/lib'));
-app.use('/', proxy('http://ec2-3-15-164-226.us-east-2.compute.amazonaws.com:3002'));
+app.use(proxy('/products', {target: 'http://ec2-3-15-164-226.us-east-2.compute.amazonaws.com:3002/'}));
 app.listen(port, () => {
   console.log(`Server running at: http://localhost:${port}`);
 });
